4bf8f1c060f0474b09b4bb200b0597b72c603b3c
Ana Marado <anammarado@gmail.com>
2025-09-10 12:17:57 +0100

    update header.tsx import on layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Header2 from "@/components/Header2";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header2 />
+        <Header />
         {children}
       </body>
     </html>
